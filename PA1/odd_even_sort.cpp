55bf39ce680826d783422e82890e319a087b6dc9
yzj <yanzj23@mails.tsinghua.edu.cn>
2025-04-15 12:18:23 +0800

    PA1: debug

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -10,28 +10,25 @@
 #include "worker.h"
 
 void Worker::sort() {
+  return;
   MPI_Request request, requests[2];
   MPI_Status status, statuses[2];
   int obj;
-  float *recv_data, *tmp;
-  
-  if (rank != nprocs - 1) {
-    recv_data = new float[block_len], tmp = new float[block_len * 2];
-  }
-
+  bool flag, recv_flag;
+  float *recv_data = new float[block_len], *tmp = new float[block_len * 2];
+ 
   for (int tag = rank & 1;; tag ^= 1) {
     if (rank != nprocs - 1 && !tag) {
       obj = rank + 1;
 
       float maximum = -std::numeric_limits<float>::infinity();
-      rep(i, 0, block_size) {
-        maximum = std::max(maximum, data[i])
+      rep(i, 0, int(block_len)) {
+        maximum = std::max(maximum, data[i]);
       }
       MPI_Isend(&maximum, 1, MPI_FLOAT, obj, 0, MPI_COMM_WORLD, &request);
 
       std::sort(data, data + block_len);
       
-      bool flag；
       MPI_Recv(&flag, 1, MPI_C_BOOL, obj, 0, MPI_COMM_WORLD, &status);
       
       if (flag) {
@@ -39,8 +36,8 @@ void Worker::sort() {
         MPI_Recv(recv_data, block_len, MPI_FLOAT, obj, 0, MPI_COMM_WORLD, &status);
         MPI_Get_count(&status, MPI_FLOAT, &count);
 
-        for (int i = 0, j = 0, k = 0; i < block_len || j < count;) {
-          if ((j >= count) || (i < block_len && data[i] <= recv_data[j])) {
+        for (int i = 0, j = 0, k = 0; i < int(block_len) || j < count;) {
+          if ((j >= count) || (i < int(block_len) && data[i] <= recv_data[j])) {
             tmp[k++] = data[i++];
           } else {
             tmp[k++] = recv_data[j++];
@@ -49,21 +46,21 @@ void Worker::sort() {
 
         MPI_Isend(tmp + block_len, count, MPI_FLOAT, obj, 0, MPI_COMM_WORLD, &request);
 
-        memcpy(data, tmp, sizeof(data));
+        memcpy(data, tmp, block_len * sizeof(float));
       }
     } else if (rank && tag) {
       obj = rank - 1;
 
       float minimum = std::numeric_limits<float>::infinity(), maximum;
-      rep(i, 0, block_size) {
-        minimum = std::min(minimum, data[i])
+      rep(i, 0, int(block_len)) {
+        minimum = std::min(minimum, data[i]);
       }
       MPI_Irecv(&maximum, 1, MPI_FLOAT, obj, 0, MPI_COMM_WORLD, &request);
 
       std::sort(data, data + block_len);
       
       MPI_Wait(&request, &status);
-      bool flag = (maximum - minimum) > EPS;
+      flag = (maximum - minimum) > EPS;
       MPI_Isend(&flag, 1, MPI_C_BOOL, obj, 0, MPI_COMM_WORLD, &request);
       
       if (flag) {
@@ -78,9 +75,9 @@ void Worker::sort() {
         MPI_Send(data, count, MPI_FLOAT, obj, 0, MPI_COMM_WORLD);
         
         MPI_Irecv(data, count, MPI_FLOAT, obj, 0, MPI_COMM_WORLD, &request);
+      }
     }
 
-    bool recv_flag;
     int source = (rank - 1 + nprocs) % nprocs, dest = (rank + 1) % nprocs;
 
     rep(i, 0, nprocs - 1) {

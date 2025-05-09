37bc92cab00a197c01002efa675405acddc1f5a0
yzj <yanzj23@mails.tsinghua.edu.cn>
2025-05-09 16:23:41 +0800

    PA2: 10000 606ms

--- PA2/apsp.cu
+++ PA2/apsp.cu
@@ -3,9 +3,8 @@
 // Brute Force APSP Implementation:
 
 #include "apsp.h"
-#include <cstdio>
 
-const int B = 64, TD = 2;
+const int K = 32, B = 64, T = 2;
 
 template <typename T>
 inline T ceiling(T x, T y) {
@@ -14,18 +13,20 @@ inline T ceiling(T x, T y) {
 
 namespace {
 
-__global__ void kernel_phase1(int p, int b, int n, int *graph) {
-    int ty = threadIdx.y * TD;
-    int tx = threadIdx.x * TD;
-    int i = p * b + ty;
-    int j = p * b + tx;
+__global__ void kernel_phase1(int p, int n, int *graph) {
+    int ty = threadIdx.y * T;
+    int tx = threadIdx.x * T;
+    int i = p * B + ty;
+    int j = p * B + tx;
 
     __shared__ int shared_block[B][B];
 
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     shared_block[ty + u][tx + v] = graph[pi * n + pj];
@@ -33,19 +34,24 @@ __global__ void kernel_phase1(int p, int b, int n, int *graph) {
     }
     __syncthreads();
 
-    int m = min(n - p * b, b);
-    #pragma unroll
+    int m = min(n - p * B, B);
+
+    #pragma unroll K
     for (int k = 0; k < m; ++k) {
-        for (int u = 0; u < TD; ++u)
-            for (int v = 0; v < TD; ++v)
+        #pragma unroll T
+        for (int u = 0; u < T; ++u)
+            #pragma unroll T
+            for (int v = 0; v < T; ++v)
                 shared_block[ty + u][tx + v] = min(shared_block[ty + u][tx + v], shared_block[ty + u][k] + shared_block[k][tx + v]);
         __syncthreads();
     }
 
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)    
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     graph[pi * n + pj] = shared_block[ty + u][tx + v];
@@ -53,30 +59,34 @@ __global__ void kernel_phase1(int p, int b, int n, int *graph) {
     }
 }
 
-__global__ void kernel_phase2_row(int p, int b, int n, int *graph) {
-    int ty = threadIdx.y * TD;
-    int tx = threadIdx.x * TD;
-    int i = p * b + ty;
-    int j = blockIdx.x * blockDim.x * TD + tx;
+__global__ void kernel_phase2_row(int p, int n, int *graph) {
+    int ty = threadIdx.y * T;
+    int tx = threadIdx.x * T;
+    int i = p * B + ty;
+    int j = blockIdx.x * blockDim.x * T + tx;
     
     if (blockIdx.x == p) return;
 
     __shared__ int shared_pivot[B][B];
-    int reg_block[TD][TD];
+    int reg_block[T][T];
 
-    for (int u = 0; u < TD; ++u) {
-        int pi = p * b + ty + u;
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
+        int pi = p * B + ty + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
-                int pj = p * b + tx + v;
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
+                int pj = p * B + tx + v;
                 if (pj < n)
                     shared_pivot[ty + u][tx + v] = graph[pi * n + pj];
             }
     }
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     reg_block[u][v] = graph[pi * n + pj];
@@ -84,19 +94,24 @@ __global__ void kernel_phase2_row(int p, int b, int n, int *graph) {
     }
     __syncthreads();
 
-    int m = min(n - p * b, b);
-    #pragma unroll
+    int m = min(n - p * B, B);
+
+    #pragma unroll K
     for (int k = 0; k < m; ++k) {
-        for (int u = 0; u < TD; ++u)
-            for (int v = 0; v < TD; ++v)
+        #pragma unroll T
+        for (int u = 0; u < T; ++u)
+            #pragma unroll T
+            for (int v = 0; v < T; ++v)
                 if (j + v < n)
-                    reg_block[u][v] = min(reg_block[u][v], shared_pivot[ty + u][k] + graph[(p * b + k) * n + j + v]);
+                    reg_block[u][v] = min(reg_block[u][v], shared_pivot[ty + u][k] + graph[(p * B + k) * n + j + v]);
     }
     
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     graph[pi * n + pj] = reg_block[u][v];
@@ -104,30 +119,34 @@ __global__ void kernel_phase2_row(int p, int b, int n, int *graph) {
     }
 }
 
-__global__ void kernel_phase2_col(int p, int b, int n, int *graph) {
-    int ty = threadIdx.y * TD;
-    int tx = threadIdx.x * TD;
-    int i = blockIdx.y * blockDim.y * TD + ty;
-    int j = p * b + tx;
+__global__ void kernel_phase2_col(int p, int n, int *graph) {
+    int ty = threadIdx.y * T;
+    int tx = threadIdx.x * T;
+    int i = blockIdx.y * blockDim.y * T + ty;
+    int j = p * B + tx;
     
     if (blockIdx.y == p) return;
 
     __shared__ int shared_pivot[B][B];
-    int reg_block[TD][TD];
+    int reg_block[T][T];
 
-    for (int u = 0; u < TD; ++u) {
-        int pi = p * b + ty + u;
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
+        int pi = p * B + ty + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
-                int pj = p * b + tx + v;
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
+                int pj = p * B + tx + v;
                 if (pj < n)
                     shared_pivot[ty + u][tx + v] = graph[pi * n + pj];
             }
     }
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     reg_block[u][v] = graph[pi * n + pj];
@@ -135,19 +154,24 @@ __global__ void kernel_phase2_col(int p, int b, int n, int *graph) {
     }
     __syncthreads();
 
-    int m = min(n - p * b, b);
-    #pragma unroll
+    int m = min(n - p * B, B);
+
+    #pragma unroll K
     for (int k = 0; k < m; ++k) {
-        for (int u = 0; u < TD; ++u)
+        #pragma unroll T
+        for (int u = 0; u < T; ++u)
             if (i + u < n)
-                for (int v = 0; v < TD; ++v)
-                    reg_block[u][v] = min(reg_block[u][v], graph[(i + u) * n + p * b + k] + shared_pivot[k][tx + v]);
+                #pragma unroll T
+                for (int v = 0; v < T; ++v)
+                    reg_block[u][v] = min(reg_block[u][v], graph[(i + u) * n + p * B + k] + shared_pivot[k][tx + v]);
     }
     
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     graph[pi * n + pj] = reg_block[u][v];
@@ -155,31 +179,35 @@ __global__ void kernel_phase2_col(int p, int b, int n, int *graph) {
     }
 }
 
-__global__ void kernel_phase3(int p, int b, int n, int *graph) {
-    int ty = threadIdx.y * TD;
-    int tx = threadIdx.x * TD;
-    int i = blockIdx.y * blockDim.y * TD + ty;
-    int j = blockIdx.x * blockDim.x * TD + tx;
+__global__ void kernel_phase3(int p, int n, int *graph) {
+    int ty = threadIdx.y * T;
+    int tx = threadIdx.x * T;
+    int i = blockIdx.y * blockDim.y * T + ty;
+    int j = blockIdx.x * blockDim.x * T + tx;
 
     if (blockIdx.y == p) return;
     if (blockIdx.x == p) return;
 
     __shared__ int shared_pivot_row[B][B], shared_pivot_col[B][B];
-    int reg_block[TD][TD];
+    int reg_block[T][T];
     
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
-                int pj = p * b + tx + v;
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
+                int pj = p * B + tx + v;
                 if (pj < n)
                     shared_pivot_row[ty + u][tx + v] = graph[pi * n + pj];
             }
     }
-    for (int u = 0; u < TD; ++u) {
-        int pi = p * b + ty + u;
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
+        int pi = p * B + ty + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     shared_pivot_col[ty + u][tx + v] = graph[pi * n + pj];
@@ -189,30 +217,34 @@ __global__ void kernel_phase3(int p, int b, int n, int *graph) {
 
     if (i >= n || j >= n) return;
     
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     reg_block[u][v] = graph[pi * n + pj];
             }
     }
     
-    int m = min(n - p * b, b);
-    #pragma unroll
-    for (int k = 0; k < m; ++k) {
-        #pragma unroll
-        for (int u = 0; u < TD; ++u)
-            #pragma unroll
-            for (int v = 0; v < TD; ++v)
+    int m = min(n - p * B, B);
+
+    #pragma unroll K
+    for (int k = 0; k < m; ++k)
+        #pragma unroll T
+        for (int u = 0; u < T; ++u)
+            #pragma unroll T
+            for (int v = 0; v < T; ++v)
                 reg_block[u][v] = min(reg_block[u][v], shared_pivot_row[ty + u][k] + shared_pivot_col[k][tx + v]);
-    }
     
-    for (int u = 0; u < TD; ++u) {
+    #pragma unroll T
+    for (int u = 0; u < T; ++u) {
         int pi = i + u;
         if (pi < n)
-            for (int v = 0; v < TD; ++v) {
+            #pragma unroll T
+            for (int v = 0; v < T; ++v) {
                 int pj = j + v;
                 if (pj < n)
                     graph[pi * n + pj] = reg_block[u][v];
@@ -223,13 +255,14 @@ __global__ void kernel_phase3(int p, int b, int n, int *graph) {
 }
 
 void apsp(int n, /* device */ int *graph) {
-    int b = B, m = ceiling(n, b);
-    dim3 thr(B / TD, B / TD);
+    int m = ceiling(n, B);
+    dim3 thr(B / T, B / T);
+
     for (int p = 0; p < m; ++p) {
-        kernel_phase1<<<dim3(1, 1), thr>>>(p, b, n, graph);
-        kernel_phase2_row<<<dim3(m, 1), thr>>>(p, b, n, graph);
-        kernel_phase2_col<<<dim3(1, m), thr>>>(p, b, n, graph);
-        kernel_phase3<<<dim3(m, m), thr>>>(p, b, n, graph);
+        kernel_phase1<<<dim3(1, 1), thr>>>(p, n, graph);
+        kernel_phase2_row<<<dim3(m, 1), thr>>>(p, n, graph);
+        kernel_phase2_col<<<dim3(1, m), thr>>>(p, n, graph);
+        kernel_phase3<<<dim3(m, m), thr>>>(p, n, graph);
     }
 }
 

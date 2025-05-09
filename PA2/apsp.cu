1c9dbf6c18f83f96596130f01a94363e6596df39
yzj <yanzj23@mails.tsinghua.edu.cn>
2025-05-09 09:58:00 +0800

    PA2: 10000 918ms 0001

--- PA2/apsp.cu
+++ PA2/apsp.cu
@@ -5,6 +5,8 @@
 #include "apsp.h"
 #include <cstdio>
 
+const int B = 32, TD = 2;
+
 template <typename T>
 inline T ceiling(T x, T y) {
     return (x + y - 1) / y;
@@ -13,41 +15,26 @@ inline T ceiling(T x, T y) {
 namespace {
 
 __global__ void kernel_phase1(int p, int b, int n, int *graph) {
-    const int TD = 1;
-    int ty = threadIdx.y * TD;
-    int tx = threadIdx.x * TD;
+    int ty = threadIdx.y;
+    int tx = threadIdx.x;
     int i = p * b + ty;
     int j = p * b + tx;
 
-    __shared__ int shared_block[32][32];
+    __shared__ int shared_block[B][B];
 
-    for (int u = 0; u < TD; ++u) {
-        int pi = i + u;
-        if (pi < n)
-            for (int v = 0; v < TD; ++v) {
-                int pj = j + v;
-                if (pj < n)
-                    shared_block[ty + u][tx + v] = graph[pi * n + pj];
-            }
+    if (i < n && j < n) {
+        shared_block[ty][tx] = graph[i * n + j];
     }
     __syncthreads();
 
     int m = min(n - p * b, b);
     for (int k = 0; k < m; ++k) {
-        for (int u = 0; u < TD; ++u)
-            for (int v = 0; v < TD; ++v)
-                shared_block[ty + u][tx + v] = min(shared_block[ty + u][tx + v], shared_block[ty + u][k] + shared_block[k][tx + v]);
+        shared_block[ty][tx] = min(shared_block[ty][tx], shared_block[ty][k] + shared_block[k][tx]);
         __syncthreads();
     }
 
-    for (int u = 0; u < TD; ++u) {
-        int pi = i + u;
-        if (pi < n)    
-            for (int v = 0; v < TD; ++v) {
-                int pj = j + v;
-                if (pj < n)
-                    graph[pi * n + pj] = shared_block[ty + u][tx + v];
-            }
+    if (i < n && j < n) {
+        graph[i * n + j] = shared_block[ty][tx];
     }
 }
 
@@ -59,7 +46,7 @@ __global__ void kernel_phase2_row(int p, int b, int n, int *graph) {
     
     if (j >= p * b) j += b;
 
-    __shared__ int shared_block[32][32], shared_pivot[32][32];
+    __shared__ int shared_block[B][B], shared_pivot[B][B];
 
     int pi = p * b + ty;
     int pj = p * b + tx;
@@ -91,7 +78,7 @@ __global__ void kernel_phase2_col(int p, int b, int n, int *graph) {
     
     if (i >= p * b) i += b;
 
-    __shared__ int shared_block[32][32], shared_pivot[32][32];
+    __shared__ int shared_block[B][B], shared_pivot[B][B];
 
     int pi = p * b + ty;
     int pj = p * b + tx;
@@ -116,7 +103,6 @@ __global__ void kernel_phase2_col(int p, int b, int n, int *graph) {
 }
 
 __global__ void kernel_phase3(int p, int b, int n, int *graph) {
-    const int TD = 2;
     int ty = threadIdx.y * TD;
     int tx = threadIdx.x * TD;
     int i = blockIdx.y * blockDim.y * TD + ty;
@@ -125,7 +111,7 @@ __global__ void kernel_phase3(int p, int b, int n, int *graph) {
     if (i >= p * b) i += b;
     if (j >= p * b) j += b;
 
-    __shared__ int shared_block[32][32], shared_pivot_row[32][32], shared_pivot_col[32][32];
+    __shared__ int shared_block[B][B], shared_pivot_row[B][B], shared_pivot_col[B][B];
     
     #pragma unroll
     for (int u = 0; u < TD; ++u) {
@@ -196,13 +182,13 @@ __global__ void kernel_phase3(int p, int b, int n, int *graph) {
 }
 
 void apsp(int n, /* device */ int *graph) {
-    int b = 32, m = ceiling(n, b);
+    int b = B, m = ceiling(n, b);
+    dim3 thr(B, B);
     for (int p = 0; p < m; ++p) {
-        dim3 thr(32, 32);
-        kernel_phase1<<<dim3(1, 1), dim3(32, 32)>>>(p, b, n, graph);
+        kernel_phase1<<<dim3(1, 1), thr>>>(p, b, n, graph);
         kernel_phase2_row<<<dim3(m - 1, 1), thr>>>(p, b, n, graph);
         kernel_phase2_col<<<dim3(1, m - 1), thr>>>(p, b, n, graph);
-        kernel_phase3<<<dim3(m - 1, m - 1), dim3(16, 16)>>>(p, b, n, graph);
+        kernel_phase3<<<dim3(m - 1, m - 1), dim3(B / TD, B / TD)>>>(p, b, n, graph);
     }
 }
 
